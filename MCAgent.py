ca6d7eefa760765653161d4dcb3b2795a84a07e4
Clemens Woest <cwoest@Clemenss-MBP.fritz.box>
2020-11-24 12:07:02 +0100

    added every-visit mc method

--- MCAgent.py
+++ MCAgent.py
@@ -56,20 +56,14 @@ class MCAgent(My10x10GridWorld):
                 G_t += reward_traj[length - 1 - t]
 
                 if N_s_per_episode[state[0], state[1]] == 0:
+                    Returns_s[state[0], state[1]] += G_t
                     N_s_per_episode[state[0], state[1]] += 1
                     N_s[state[0], state[1]] += 1
-                    Returns_s[state[0], state[1]] += G_t
 
             self.v = np.divide(Returns_s, N_s, out=np.zeros_like(Returns_s), where=N_s != 0)
 
         self.v = np.divide(Returns_s, N_s, out=np.zeros_like(Returns_s), where=N_s!=0)
 
-    def runFirstVisitMonteCarlo(self, episodes = 10):
-        """Runs first-visit Monte-Carlo Method"""
-
-        self.firstVisitMCPrediction(episodes)
-        self.policyImprovement()
-        print(self.pi)
 
     def everyVisitMCPrediction(self, episodes):
         """
@@ -89,8 +83,6 @@ class MCAgent(My10x10GridWorld):
 
             print(f"-- episode={episode}\n{np.round(self.v, 2)}")
 
-            # counter for each state per episode - to check first-visits
-            N_s_per_episode = np.zeros((self.NROWS, self.NCOLS))
             state = self.starting_state
             state_traj, action_traj, reward_traj = np.empty((0, 2), dtype=int), np.empty(0), np.empty(0)
 
@@ -111,18 +103,32 @@ class MCAgent(My10x10GridWorld):
             action_traj = np.append(action_traj, self.A[np.random.choice(np.array(range(0, len(self.A))), size=1)[0]])
             reward_traj = np.append(reward_traj, self.getRewardForAction(state))
 
+            # to get the correct indice in reverse order
+            length = len(reward_traj)
+
             G_t = 0
             for t, state in enumerate(reversed(state_traj)):
-                G_t += reward_traj[t]
+                G_t += reward_traj[length - 1 - t]
+                Returns_s[state[0], state[1]] += G_t
+                N_s[state[0], state[1]] += 1
 
-                if N_s_per_episode[state[0], state[1]] == 0:
-                    N_s_per_episode[state[0], state[1]] += 1
-                    N_s[state[0], state[1]] += 1
-                    Returns_s[state[0], state[1]] += G_t
-
-            self.v = np.divide(Returns_s, N_s, out=np.zeros_like(Returns_s), where=N_s != 0)
+            self.v = np.divide(Returns_s, N_s, out=np.zeros_like(Returns_s), where=N_s!=0)
 
         self.v = np.divide(Returns_s, N_s, out=np.zeros_like(Returns_s), where=N_s!=0)
+
+    def runFirstVisitMonteCarlo(self, episodes = 10):
+        """Runs first-visit Monte-Carlo Method"""
+
+        self.firstVisitMCPrediction(episodes)
+        self.policyImprovement()
+        print(self.pi)
+
+    def runEveryVisitMonteCarlo(self, episodes = 10):
+        """Runs every-visit Monte-Carlo Method"""
+
+        self.everyVisitMCPrediction(episodes)
+        self.policyImprovement()
+        print(self.pi)
 
 
 """Let the agent reinforce"""
@@ -130,4 +136,10 @@ mc_agent = MCAgent([NROWS, NCOLS], starting_state, terminal_states, A,
                    rewards, neg_reward_states, pos_reward_states, Walls,
                    Gamma, v, pi, piProbs, eps, Alpha, epsilon)
 
-mc_agent.runFirstVisitMonteCarlo(20)
+whatToDo = input("Press 1 for First-Visit Monte-Carlo:\n"
+                 "      2 for Every-Visit Monte-Carlo: ")
+
+if whatToDo == "1":
+    mc_agent.runFirstVisitMonteCarlo(20)
+elif whatToDo == "2":
+    mc_agent.runEveryVisitMonteCarlo(20)
